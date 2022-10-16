ce8cc50506d116d9eaba92f714edf3883757d9d0
ira <lilrlal@yandex.ru>
2022-10-16 16:45:47 +0300

    dz2

--- final task/Program.cs
+++ final task/Program.cs
@@ -1,14 +1,14 @@
-﻿string[] array = new string[10] {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
+﻿string[] array = new string[10] { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten" };
 string[] shortWord = new string[array.Length];
-void SecondArrayWithIF(string[] array, string[] array2)
+void CheckWord(string[] array, string[] array2)
 {
     int count = 0;
     for (int i = 0; i < array.Length; i++)
     {
-    if(array[i].Length <= 3)
+        if (array[i].Length <= 3)
         {
-        shortWord[count] = array[i];
-        count++;
+            shortWord[count] = array[i];
+            count++;
         }
     }
 }
@@ -16,9 +16,10 @@ void PrintArray(string[] array)
 {
     for (int i = 0; i < array.Length; i++)
     {
-        Console.Write($"{array[i]} ");
+        Console.Write($" {array[i]} ");
     }
     Console.WriteLine();
 }
-SecondArrayWithIF(array, shortWord);
+CheckWord(array, shortWord);
+Console.WriteLine("Данный массив содержит следующие слова короче трех символов:");
 PrintArray(shortWord);
